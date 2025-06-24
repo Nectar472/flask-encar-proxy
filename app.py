c218ee6f98a7ccd32fce0fbc0b4860a581ff6b47
Nectar472 <kimkonstantin42@gmail.com>
2025-06-24 16:41:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,6 @@ proxy = EncarBackupProxy()
 @app.get("/api/catalog")
 async def proxy_catalog(q: str = Query(...), sr: str = Query(...)):
     url = f"https://api.encar.com/search/car/list?count=true&q={q}&sr={sr}"
-"
     result = await proxy.request(url)
 
     if result.get("success"):
